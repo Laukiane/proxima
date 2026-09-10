2241e797ead9788718fe2fccde53c2e7e65ac6dd
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-10 17:15:27 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ import { state } from './state.js';
 import { CLIENTS } from '../data/clients.js';
 import { DOSSIERS } from '../data/dossiers.js';
 import { Rules } from './rules.js';
+import { applyWorkflow } from './workflow.js';
 
 console.log("STATE =", state);
 console.log("NB CLIENTS =", CLIENTS.length);
 console.log("NB DOSSIERS =", DOSSIERS.length);
 console.log("RULES =", Rules);
+console.log("WORKFLOW =", applyWorkflow);
