2e40129b5da3c724dd86923706d8ef5431e3a4e4
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-10 17:24:12 +0200

    Enhance logging with ROUTES and renderView
    Added ROUTES and renderView to console logs.

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ import { CLIENTS } from '../data/clients.js';
 import { DOSSIERS } from '../data/dossiers.js';
 import { Rules } from './rules.js';
 import { applyWorkflow } from './workflow.js';
+import { ROUTES, renderView } from './router.js';
 
 console.log("STATE =", state);
 console.log("NB CLIENTS =", CLIENTS.length);
 console.log("NB DOSSIERS =", DOSSIERS.length);
 console.log("RULES =", Rules);
 console.log("WORKFLOW =", applyWorkflow);
+console.log("ROUTES =", ROUTES);
+console.log("RENDERVIEW =", renderView);
