ab141374bd906912c5c35e23e98a91e27ea9f274
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-10 16:54:30 +0200

    Import DOSSIERS and log its count
    Added import for DOSSIERS and logged its length.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import { state } from './state.js';
 import { CLIENTS } from '../data/clients.js';
+import { DOSSIERS } from '../data/dossiers.js';
 
 console.log("STATE =", state);
 console.log("NB CLIENTS =", CLIENTS.length);
+console.log("NB DOSSIERS =", DOSSIERS.length);
