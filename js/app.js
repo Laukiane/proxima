4d3970a5c0b5094a38438bfd192001d2c7ea68c1
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-10 17:00:21 +0200

    Add Rules import and log to app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import { state } from './state.js';
 import { CLIENTS } from '../data/clients.js';
 import { DOSSIERS } from '../data/dossiers.js';
+import { Rules } from './rules.js';
 
 console.log("STATE =", state);
 console.log("NB CLIENTS =", CLIENTS.length);
 console.log("NB DOSSIERS =", DOSSIERS.length);
+console.log("RULES =", Rules);
