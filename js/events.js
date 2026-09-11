0cf07b31aa6e91c8863e8deeedf9fe073275b9e9
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-11 17:36:23 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -2,11 +2,7 @@ export function registerEvents(){
 
   document.addEventListener('click', (e)=>{
 
-    const t = e.target.closest('[data-nav-to]');
-
-    if(t){
-      console.log('NAV TO =', t.dataset.navTo);
-    }
+    console.log('CLICK', e.target);
 
   });
 
