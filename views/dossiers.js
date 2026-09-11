96773e895540ce89327eab583145244ac92594d5
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-11 14:37:10 +0200

    Update dossiers.js

--- views/dossiers.js
+++ views/dossiers.js
@@ -1,3 +1,10 @@
+import { state } from '../js/state.js';
+
+import {
+  visibleDossiers,
+  dossierTable
+} from '../js/utils.js';
+
 export function viewDossiers(){
   let list = visibleDossiers();
   if(state.role==='assistant') list = list.slice().sort((a,b)=> (a.statut==='soumis'||a.statut==='anomalie'?-1:1) - (b.statut==='soumis'||b.statut==='anomalie'?-1:1));
