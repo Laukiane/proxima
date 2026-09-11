ebaaecbfba387de092fc722c0d428f3dd48dce24
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-11 15:06:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ import { INDEXES } from '../data/indexes.js';
 import { idx } from './utils.js';
 import { EXCEPTIONS_TAUX } from '../data/exceptions-taux.js';
 import { findExceptionTaux } from './utils.js';
+import { dossierActionsFor } from './workflow.js';
+
 
 console.log("STATE =", state);
 console.log("NB CLIENTS =", CLIENTS.length);
@@ -40,3 +42,4 @@ console.log("NB INDEXES =", INDEXES.length);
 console.log("IDX =", idx);
 console.log("NB EXCEPTIONS =", EXCEPTIONS_TAUX.length);
 console.log("FIND EXCEPTION =", findExceptionTaux);
+console.log("DOSSIER ACTIONS =", dossierActionsFor);
