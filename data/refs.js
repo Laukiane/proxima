486551f3c256d9d6f79d135256f989f73b83110d
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-11 12:17:12 +0200

    Add REFS array with product information

--- data/refs.js
+++ data/refs.js
@@ -1,1 +1,13 @@
-
+export const REFS = [
+  {ref:'PN-2201', nom:'Pneu Continental EcoContact 205/55R16', marque:'Continental', famille:'Pneus', indexId:'IDX1'},
+  {ref:'PN-2202', nom:'Pneu Michelin Primacy 195/65R15', marque:'Michelin', famille:'Pneus', indexId:'IDX1'},
+  {ref:'FRQ-3390', nom:'Plaquettes de frein Bosch avant', marque:'Bosch', famille:'Freinage', indexId:'IDX3'},
+  {ref:'AMZ-1187', nom:'Amortisseur arrière Sachs', marque:'Sachs', famille:'Suspension', indexId:'IDX4'},
+  {ref:'FLT-0552', nom:'Filtre à huile Mann', marque:'Mann Filter', famille:'Filtration', indexId:'IDX5'},
+  {ref:'BAT-7742', nom:'Batterie Varta 74Ah', marque:'Varta', famille:'Électrique', indexId:'IDX6'},
+  {ref:'RAD-2290', nom:'Radiateur Valeo', marque:'Valeo', famille:'Refroidissement', indexId:'IDX7'},
+  {ref:'EMB-6631', nom:'Kit embrayage LuK', marque:'LuK', famille:'Transmission', indexId:'IDX8'},
+  {ref:'BAT-ERP-7001', nom:'Batterie EUROREPAR 74Ah', marque:'EUROREPAR', famille:'EUROREPAR', indexId:'IDX9'},
+  {ref:'HUI-ERP-5001', nom:"Fût d'huile moteur EUROREPAR 20L", marque:'EUROREPAR', famille:'EUROREPAR', indexId:'IDX9'},
+  {ref:'FLT-PURFLUX-9001', nom:'Filtre à huile PURFLUX', marque:'PURFLUX', famille:'EQT', indexId:'IDX10'},
+];
