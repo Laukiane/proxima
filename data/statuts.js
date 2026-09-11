faea4ae3840ecda9482bd97b57c1eb086a836a26
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-11 11:48:17 +0200

    Create statuts.js

---
+++ data/statuts.js
@@ -0,0 +1,11 @@
+export const STATUT_LABELS = {
+  brouillon:['Brouillon','b-gray'],
+  soumis:['Soumis','b-blue'],
+  anomalie:['Anomalie détectée','b-red'],
+  controle_en_cours:['Contrôle en cours','b-orange'],
+  correction:['Correction demandée','b-orange'],
+  refuse:['Refusé','b-red'],
+  valide_assistant:['Validé assistant commerce','b-blue'],
+  valide_responsable:['Validé responsable commerce','b-blue'],
+  archive:['Archivé','b-green'],
+};
