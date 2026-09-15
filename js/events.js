01dffbe84f4d6f29d9a837e2e82d65fcbae6ee21
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-15 10:46:24 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -41,18 +41,7 @@ export function registerEvents() {
     const gainCard = e.target.closest('[data-select-gain]');
 
     if (gainCard) {
-      const id = gainCard.dataset.selectGain;
-      const i = state.wizard.gains.indexOf(id);
-
-      console.log('SELECT GAIN', id);
-
-      if (i === -1) {
-        state.wizard.gains.push(id);
-      } else {
-        state.wizard.gains.splice(i, 1);
-      }
-
-      renderView();
+      console.log('SELECT GAIN', gainCard.dataset.selectGain);
     }
 
     const actionButton = e.target.closest('[data-action]');
@@ -62,66 +51,4 @@ export function registerEvents() {
       startWizard();
     }
 
-    if (actionButton?.dataset.action === 'new-dossier-for') {
-      console.log('NEW DOSSIER FOR', actionButton.dataset.client);
-      startWizard(actionButton.dataset.client);
-    }
-
-    if (actionButton?.dataset.action === 'print-dossier') {
-      console.log('PRINT DOSSIER', actionButton.dataset.num);
-      printDossier(actionButton.dataset.num);
-    }
-
-    if (actionButton?.dataset.action === 'view-gain-doc') {
-      console.log('VIEW GAIN DOC');
-
-      const d = DOSSIERS.find(x => x.num === actionButton.dataset.num);
-      const doc = d && d.documents[Number(actionButton.dataset.doc)];
-
-      if (doc) {
-        openGainDocPreviewModal(doc);
-      }
-    }
-
-    if (actionButton?.dataset.action === 'save-draft') {
-      console.log('SAVE DRAFT MIGRATION OK');
-    }
-
-    if (actionButton?.dataset.action === 'submit-wizard') {
-      console.log('SUBMIT WIZARD');
-      openSubmitConfirmModal();
-    }
-
-    if (actionButton?.dataset.action === 'confirm-submit-wizard') {
-      console.log('CONFIRM SUBMIT WIZARD MIGRATION OK');
-    }
-
-    if (actionButton?.dataset.action === 'correct-dossier') {
-      console.log('CORRECT DOSSIER', actionButton.dataset.num);
-      correctDossier(actionButton.dataset.num);
-    }
-
-    if (actionButton?.dataset.action === 'resume-draft') {
-      console.log('RESUME DRAFT', actionButton.dataset.num);
-      resumeDraft(actionButton.dataset.num);
-    }
-
-    if (e.target.dataset.wizardNext !== undefined) {
-      console.log('WIZARD NEXT');
-    }
-
-    if (e.target.dataset.wizardPrev !== undefined) {
-      console.log('WIZARD PREV');
-    }
-
-    if (e.target.dataset.closeModal !== undefined) {
-      console.log('CLOSE MODAL');
-      closeModal();
-    }
-
-    if (e.target.id === 'modal-overlay') {
-      console.log('OVERLAY CLOSE');
-      closeModal();
-    }
-  });
-}
+    if (actionButton?.dataset.action === 
