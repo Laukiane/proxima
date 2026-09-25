7ae38bfa1a2f8eaa4e4f07a404445ff4f4dfe557
Laukiane <laukiane.simoespereira@stellantis.com>
2026-09-25 16:37:41 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -20,7 +20,9 @@ export function registerEvents() {
 
     if (dossierRow) {
       console.log('OPEN DOSSIER =', dossierRow.dataset.openDossier);
-      nav('dossierDetail', { dossierDetailNum: dossierRow.dataset.openDossier });
+      nav('dossierDetail', {
+        dossierDetailNum: dossierRow.dataset.openDossier
+      });
     }
 
     const workflowButton = e.target.closest('[data-workflow]');
@@ -45,7 +47,10 @@ export function registerEvents() {
     const gainCard = e.target.closest('[data-select-gain]');
 
     if (gainCard) {
-      console.log('SELECT GAIN', gainCard.dataset.selectGain);
+      console.log(
+        'SELECT GAIN',
+        gainCard.dataset.selectGain
+      );
     }
 
     const wizardClientRow = e.target.closest(
@@ -62,7 +67,10 @@ export function registerEvents() {
     const lineCheckbox = e.target.closest('[data-line]');
 
     if (lineCheckbox) {
-      console.log('SELECT LINE', lineCheckbox.dataset.line);
+      console.log(
+        'SELECT LINE',
+        lineCheckbox.dataset.line
+      );
     }
 
     const actionButton = e.target.closest('[data-action]');
@@ -72,4 +80,184 @@ export function registerEvents() {
       startWizard();
     }
 
-    if
+    if (actionButton?.dataset.action === 'new-dossier-for') {
+      console.log(
+        'NEW DOSSIER FOR',
+        actionButton.dataset.client
+      );
+
+      startWizard(actionButton.dataset.client);
+    }
+
+    if (actionButton?.dataset.action === 'print-dossier') {
+      console.log(
+        'PRINT DOSSIER',
+        actionButton.dataset.num
+      );
+
+      printDossier(actionButton.dataset.num);
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'download-ventes-template'
+    ) {
+      console.log('DOWNLOAD VENTES TEMPLATE');
+      downloadVentesTemplate();
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'reset-ventes'
+    ) {
+      console.log('RESET VENTES');
+      resetVentesDefaut();
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'view-gain-doc'
+    ) {
+      console.log('VIEW GAIN DOC');
+
+      const d = DOSSIERS.find(
+        (x) => x.num === actionButton.dataset.num
+      );
+
+      const doc =
+        d &&
+        d.documents[
+          Number(actionButton.dataset.doc)
+        ];
+
+      if (doc) {
+        openGainDocPreviewModal(doc);
+      }
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'change-wizard-client'
+    ) {
+      console.log('CHANGE WIZARD CLIENT');
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'save-draft'
+    ) {
+      console.log('SAVE DRAFT MIGRATION OK');
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'submit-wizard'
+    ) {
+      console.log('SUBMIT WIZARD');
+      openSubmitConfirmModal();
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'confirm-submit-wizard'
+    ) {
+      console.log(
+        'CONFIRM SUBMIT WIZARD MIGRATION OK'
+      );
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'correct-dossier'
+    ) {
+      console.log(
+        'CORRECT DOSSIER',
+        actionButton.dataset.num
+      );
+
+      correctDossier(actionButton.dataset.num);
+    }
+
+    if (
+      actionButton?.dataset.action ===
+      'resume-draft'
+    ) {
+      console.log(
+        'RESUME DRAFT',
+        actionButton.dataset.num
+      );
+
+      resumeDraft(actionButton.dataset.num);
+    }
+
+    if (
+      e.target.dataset.wizardNext !== undefined
+    ) {
+      console.log('WIZARD NEXT');
+    }
+
+    if (
+      e.target.dataset.wizardPrev !== undefined
+    ) {
+      console.log('WIZARD PREV');
+    }
+
+    if (
+      e.target.dataset.closeModal !== undefined
+    ) {
+      console.log('CLOSE MODAL');
+      closeModal();
+    }
+
+    if (e.target.id === 'modal-overlay') {
+      console.log('OVERLAY CLOSE');
+      closeModal();
+    }
+  });
+
+  document.addEventListener('change', (e) => {
+    if (e.target.id === 'participation-check') {
+      console.log(
+        'PARTICIPATION CHANGE',
+        e.target.checked
+      );
+
+      if (state.wizard) {
+        state.wizard.participation =
+          e.target.checked;
+      }
+    }
+
+    if (e.target.dataset.gainCheck) {
+      console.log(
+        'GAIN CHECK',
+        e.target.dataset.gainCheck,
+        e.target.dataset.num,
+        e.target.checked
+      );
+    }
+
+    if (e.target.id === 'ventes-import-input') {
+      console.log(
+        'VENTES IMPORT',
+        e.target.files?.[0]?.name
+      );
+    }
+
+    if (e.target.dataset.justifUpload) {
+      console.log(
+        'JUSTIF UPLOAD',
+        e.target.dataset.justifUpload,
+        e.target.files?.[0]?.name
+      );
+    }
+
+    if (e.target.dataset.gainDocUpload) {
+      console.log(
+        'GAIN DOC UPLOAD',
+        e.target.dataset.gainDocUpload,
+        e.target.files?.[0]?.name
+      );
+    }
+  });
+}
